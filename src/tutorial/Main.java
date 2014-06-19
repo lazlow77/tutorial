41a44c5343b11219cd97ccb2da5992f97e15b797
James Hughes <jameshughes77@gmail.com>
2014-06-19 12:22:59 +0100

    First commit

---
+++ src/tutorial/Main.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        //Enemy objects
+        String enemy[] = { "Skeleton", "Zombie", "Warrior", "Assassin"};
+
+        System.out.println(enemy[0]);
+
+    }
+
+}
